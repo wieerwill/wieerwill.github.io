08cb1264c8b01d0e95eb9df23bdb277b786b1b56
Robert Jeutter <48153869+wieerwill@users.noreply.github.com>
2019-03-18 20:11:10 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+$("#pro").hover(
+	function(){
+  $("#pro1").css("display", "none");
+  $("#pro2").css("display", "block");
+  }, function(){
+  $("#pro1").css("display", "block");
+  $("#pro2").css("display", "none");
+});
+
+$("#full").hover(
+	function(){
+  $("#full1").css("display", "none");
+  $("#full2").css("display", "block");
+  }, function(){
+  $("#full1").css("display", "block");
+  $("#full2").css("display", "none");
+});
+
+$("#cad").hover(
+	function(){
+  $("#cad1").css("display", "none");
+  $("#cad2").css("display", "block");
+  }, function(){
+  $("#cad1").css("display", "block");
+  $("#cad2").css("display", "none");
+});
