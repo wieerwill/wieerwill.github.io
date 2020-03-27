1d18344f3834dfe6b9e2191e9e41427cd5f78655
Robert Jeutter <robert.jeutter@gmx.de>
2020-03-27 18:35:25 +0100

    refreshed styling; better overview; projects list and certificates;

--- index.js
+++ index.js
@@ -24,3 +24,24 @@ $("#cad").hover(
   $("#cad1").css("display", "block");
   $("#cad2").css("display", "none");
 });
+
+
+//Certification show
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("modalImage1");
+//var img = document.getElementById("img1");
+
+img.onclick = function () {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+
+function modalImg() {
+    modal.style.display = "block";
+    modalImg.src = this.src;
+}
+// When the user clicks on <span> (x), close the modal
+var span = document.getElementsByClassName("close")[0];
+span.onclick = function () {
+    modal.style.display = "none";
+}
